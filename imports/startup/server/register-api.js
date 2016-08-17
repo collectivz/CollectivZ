3e91ed22dd9fe527b969557043d67dfe44de22fa
pdecrat <philippe.decrat@gmail.com>
2016-08-17 14:27:03 +0200

    api beers: collection and insert

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -5,12 +5,12 @@ import '../../api/guilds/methods.js';
 import '../../api/guilds/server/publish.js';
 
 import '../../api/messages/methods.js';
-import '../../api/messages/server/publish.js';
 
 import '../../api/polls/methods.js';
-import '../../api/polls/server/publish.js';
 
 import '../../api/users/users.js';
 import '../../api/users/server/publish.js';
+
+import '../../api/beers/methods.js';
 
 import '../../api/methods.js';
