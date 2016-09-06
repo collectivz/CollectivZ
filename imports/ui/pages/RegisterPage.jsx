a49b7306a798492807c924327340d3b9d82d20b1
pdecrat <philippe.decrat@gmail.com>
2016-09-06 14:05:00 +0200

    registerPage

---
+++ imports/ui/pages/RegisterPage.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+
+export default class RegisterPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      errors: []
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.errors);
+  }
+
+  handleSubmit(e) {
+    const errors = [];
+    const mailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const username = this.refs.username.value;
+    const email = this.refs.email.value;
+    const password = this.refs.password.value;
+    const passwordAgain = this.refs.passwordAgain.value;
+
+    e.preventDefault();
+
+    if (!username) {
+      errors.push("Entrez un nom d'utilisateur.");
+    }
+    if (!email) {
+      errors.push("Entrez un email.");
+    }
+    if (!mailRegex.test(email)) {
+      errors.push("Entrez un email valide.");
+    }
+    if (!password || !passwordAgain) {
+      errors.push("Entrez et confirmez votre mot de passe.");
+    }
+    if (password.length < 6) {
+      errors.push("Votre mot de passe est trop court.")
+    }
+    if (password !== passwordAgain) {
+      errors.push("Le mot de passe ne correspond pas à la confirmation.")
+    }
+
+    if (errors.length) {
+      this.setState({
+        errors
+      }, () => {
+        console.log(this.state);
+      });
+    } else {
+      Accounts.createUser({email, username, password}, (err) => {
+        if (err) {
+          this.setState({
+            errors: [err.reason]
+          });
+        }
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <form id="box" onSubmit={this.handleSubmit}>
+          <div className="inner">
+            <input
+              type="text"
+              placeholder="Username"
+              ref="username"
+            />
+            <input
+              type="text"
+              placeholder="Email"
+              ref="email"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              ref="password"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              ref="passwordAgain"
+            />
+            <input type="submit" value="Register" />
+          </div>
+
+        </form>
+
+      </div>
+    );
+  }
+}
