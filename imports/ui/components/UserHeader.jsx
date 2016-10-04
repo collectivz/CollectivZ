243485b5651dcddfbaf97acb18992d5648efc34f
Thibaud FRERE <thibaud.frere@gmail.com>
2016-10-04 13:24:35 +0200

    ajoute someoneistyping | rends le admin propre | ajoute la list-item-action class

--- imports/ui/components/UserHeader.jsx
+++ imports/ui/components/UserHeader.jsx
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Meteor } from 'meteor/meteor';
+import UploadPicture                            from './UploadPicture.jsx';
 
 export default class UserHeader extends Component {
 
@@ -9,6 +10,10 @@ export default class UserHeader extends Component {
 
     return (
       <div style={{ backgroundImage: `url('${user.profile.background}')` }} className="profile">
+        {/*
+        // Linker l'upload de picture au src de l'img du profil pour voir en direct
+        // le changement
+        <UploadPicture data={user} method="users.changeAvatar"/>*/}
           <div className="profile-header">
               <img src={user.profile.avatar}/>
               <h3>{user.username}</h3>
