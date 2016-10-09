5ff10b64eea02698aaceeee329f66eb18e71f71c
Philippe Decrat <philippe.decrat@gmail.com>
2016-10-09 21:36:40 +0200

    log added in messageItemContainer

--- imports/ui/containers/MessageItemContainer.jsx
+++ imports/ui/containers/MessageItemContainer.jsx
@@ -6,6 +6,7 @@ import MessageItem from '../components/chat/MessageItem';
 export default createContainer(({ message }) => {
   const author = Meteor.users.findOne(message.author);
 
+  console.log(author);
   return {
     author
   };
