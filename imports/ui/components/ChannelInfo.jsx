18335498d99b8039544253841adb90431cc9ad75
Philippe Decrat <philippe.decrat@gmail.com>
2016-11-16 23:43:28 +0100

    fix description

--- imports/ui/components/ChannelInfo.jsx
+++ imports/ui/components/ChannelInfo.jsx
@@ -19,7 +19,7 @@ export default class ChannelInfo extends React.Component {
           : ''
         }
         <h4>{channel.name}</h4>
-        {(channel.description) ? <p>channel.description</p> : <p className="grey-text">Ce groupe n'a pas de description.</p> }
+        {(channel.description) ? <p>{channel.description}</p> : <p className="grey-text">Ce groupe n'a pas de description.</p> }
         <div className="list-sub-menu small white">
             <i className="big-icon icon icon-members"/>
             <h5>Liste des membres</h5>
