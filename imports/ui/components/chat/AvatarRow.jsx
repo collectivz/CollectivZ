8787e020fcf11cbb6ac351d178845b9653070097
Philippe Decrat <philippe.decrat@gmail.com>
2016-11-18 02:10:12 +0100

    fix ???

--- imports/ui/components/chat/AvatarRow.jsx
+++ imports/ui/components/chat/AvatarRow.jsx
@@ -14,7 +14,7 @@ export default class AvatarRow extends React.Component {
   render() {
 
     let avatars = this.props.avatars;
-    let numberOfUsers = this.props.avatars.length;
+    let numberOfUsers = avatar ? avatars.length : 0;
     let hasToMuchUsers = false;
     if (numberOfUsers >= 4) {
       avatars = avatars.slice(0, 2);
