18484d35f1a224d85a1868b078a1f97d3d25e24e
jacquesbonet <jacques.bonet@gmail.com>
2017-01-27 00:05:07 +0100

    - inAppBrowser url

--- imports/api/messages/url-handler.js
+++ imports/api/messages/url-handler.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import jsxToString from 'jsx-to-string';
 
 
-const urlRegex = new RegExp(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi);
+const urlRegex = new RegExp(/[-a-zA-Z0-9@:%_+.~#?&/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_+.~#?&//=]*)?/gi);
 
 export function replaceUrls(string) {
   const target = string.replace(urlRegex, (match) => {
     if (match[0] !== 'h') {
-      return 'http://{target}';
+      return `http://${match}`;
     }
-    return target;
+    return match;
   });
+  const openUrl = aTarget => (`window.open( ${aTarget}, "_system")`);
 
-  return jsxToString(<a href={window.open(target, '_system')} target="_system">{target}</a>);
+  return `<a href=${openUrl(target, '_system')} target="_system">${target}</a>`;
 }
