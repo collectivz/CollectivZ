46d0fad3e80ef82934666fa9a75f2801363a3216
jacquesbonet <jacques.bonet@gmail.com>
2017-01-26 21:20:07 +0100

    - inAppBrowser url

--- imports/api/messages/url-handler.js
+++ imports/api/messages/url-handler.js
@@ -2,10 +2,14 @@ const urlRegex = new RegExp(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[
 
 export function replaceUrls(string) {
   const newString = string.replace(urlRegex, (match) => {
+    let newMatch = '';
     if (match[0] !== 'h') {
-      match = `http://${match}`;
+      newMatch = `http://${match}`;
+    } else {
+      newMatch = match;
     }
-    return `<a href="${match}" target="_system">${match}</a>`;
+    const target = `window.open(${newMatch}, "_system")`;
+    return `<a href="#" target=${target}>${newMatch}</a>`;
   });
 
   return newString;
--- imports/startup/client/mobile-url-manager.js
+++ imports/startup/client/mobile-url-manager.js
@@ -1,18 +1,4 @@
 import { Meteor } from 'meteor/meteor';
-import $ from 'jquery';
 
 Meteor.startup(() => {
-  $(document).on('deviceready', () => {
-    $(document).on('click', (e) => {
-//      if (Meteor.isCordova) {
-      const atts = e.currentTarget.attributes;
-      if (typeof atts.target !== 'undefined') {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-        e.stopPropagation();
-        window.open(atts.href.value, atts.target.value);
-      }
-//      }
-    });
-  });
 });
