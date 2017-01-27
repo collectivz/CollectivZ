811a5817cc492908ba91ab4ef3f361802fa3f3e5
jacquesbonet <jacques.bonet@gmail.com>
2017-01-27 01:46:39 +0100

    - inAppBrowser url

--- imports/startup/client/mobile-url-manager.js
+++ imports/startup/client/mobile-url-manager.js
@@ -1,4 +1,10 @@
-import { Meteor } from 'meteor/meteor';
+import { Meteor } from 'meteor';
+import { $ } from 'meteor/underscore';
 
 Meteor.startup(() => {
+   if (Meteor.isCordova) {
+      $(document).on('deviceready', () => {
+         window.open = cordova.InAppBrowser.open;
+      });
+   }
 });
