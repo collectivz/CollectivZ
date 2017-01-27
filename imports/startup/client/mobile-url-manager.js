eee14c6aa89c68e167492df19907faa47b265592
jacquesbonet <jacques.bonet@gmail.com>
2017-01-27 15:20:53 +0100

    - inAppBrowser url

--- imports/startup/client/mobile-url-manager.js
+++ imports/startup/client/mobile-url-manager.js
@@ -1,7 +1,7 @@
 Template.Layout.events({
-   'click a.system-browser': function(evt) {
-      evt.preventDefault();
-      var href = $(evt.target).closest('a').attr('href');
-      window.open(href, '_system', 'location=yes');
-   }
+  'click a.system-browser': (evt) => {
+    evt.preventDefault();
+    const href = $(evt.target).closest('a').attr('href');
+    window.open(href, '_blank', 'location=yes');
+  },
 });
