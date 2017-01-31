1cc4eab9b3e86f4f4230a29c0a523d075c1ad019
jacquesbonet <jacques.bonet@gmail.com>
2017-01-31 05:32:38 +0100

    - inAppBrowser url

--- imports/api/messages/url-handler.js
+++ imports/api/messages/url-handler.js
@@ -11,5 +11,5 @@ export function replaceUrls(string) {
     return match;
   });
 
-  return `<a href="#" onclick="window.open( ${target}, '_system');">${target}</a>`;
+  return `<a href="#" onclick="window.open( '${target}', '_system');">${target}</a>`;
 }
