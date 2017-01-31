b6e4be3ccdcc2d9929ca0c43f02528b536cad311
jacquesbonet <jacques.bonet@gmail.com>
2017-01-31 05:10:11 +0100

    - inAppBrowser url

--- imports/api/messages/url-handler.js
+++ imports/api/messages/url-handler.js
@@ -1,3 +1,6 @@
+import React from 'react';
+
+
 const urlRegex = new RegExp(/[-a-zA-Z0-9@:%_+.~#?&/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_+.~#?&//=]*)?/gi);
 
 export function replaceUrls(string) {
@@ -7,7 +10,6 @@ export function replaceUrls(string) {
     }
     return match;
   });
-  const openUrl = aTarget => (`window.open( ${aTarget}, '_system')`);
 
-  return `<a href="#" onclick=${openUrl(target, '_system')}>${target}</a>`;
+  return `<a href="#" onclick="window.open( ${target}, '_system');">${target}</a>`;
 }
