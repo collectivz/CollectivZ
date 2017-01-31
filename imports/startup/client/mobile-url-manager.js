9ebe81237f87b48c0aa68aeadf57472f8f1a661d
jacquesbonet <jacques.bonet@gmail.com>
2017-01-31 01:12:33 +0100

    - inAppBrowser url

--- imports/startup/client/mobile-url-manager.js
+++ imports/startup/client/mobile-url-manager.js
@@ -2,15 +2,16 @@ Meteor.startup(() => {
   if (!Meteor.isCordova) {
     return;
   }
-  return $(document).on('deviceready', () => $(document).on('click', (e) => {
-    let $link = $(e.target).closest('a[href]');
-    if (!($link.length > 0)) {
-      return;
-    }
-    let url = $link.attr('href');
-    if (/^https?:\/\/.+/.test(url) === true) {
-      cordova.InAppBrowser.open(url, '_system');
-      return e.preventDefault();
-    }
-  }));
+  return $(document).on('deviceready', () =>
+     $(document).on('click', (e) => {
+       const $link = $(e.target).closest('a[href]');
+       if (!($link.length > 0)) {
+         return;
+       }
+       const url = $link.attr('href');
+       if (/^https?:\/\/.+/.test(url) === true) {
+         cordova.InAppBrowser.open(url, '_system');
+         return e.preventDefault();
+       }
+     }));
 });
