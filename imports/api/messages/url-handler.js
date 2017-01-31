38d3951403f5d5170a8ef66c0f00307f446c9af6
jacquesbonet <jacques.bonet@gmail.com>
2017-01-31 12:52:56 +0100

    - inAppBrowser url

--- imports/api/messages/url-handler.js
+++ imports/api/messages/url-handler.js
@@ -1,6 +1,3 @@
-import React from 'react';
-
-
 const urlRegex = new RegExp(/[-a-zA-Z0-9@:%_+.~#?&/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_+.~#?&//=]*)?/gi);
 
 export function replaceUrls(string) {
@@ -11,5 +8,5 @@ export function replaceUrls(string) {
     return match;
   });
 
-  return `<a href="#" onclick="cordova.InAppBrowser.open( '${target}', '_system');">${target}</a>`;
+  return `<a href="#" onclick="window.open( '${target}', '_system');">${target}</a>`;
 }
