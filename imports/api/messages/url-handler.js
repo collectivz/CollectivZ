496ac557ac43d373912cb989f0c54b02632e2eb1
jacquesbonet <jacques.bonet@gmail.com>
2017-02-05 01:29:28 +0100

    - test server

--- imports/api/messages/url-handler.js
+++ imports/api/messages/url-handler.js
@@ -3,10 +3,9 @@ const urlRegex = new RegExp(/[-a-zA-Z0-9@:%_+.~#?&/=]{2,256}\.[a-z]{2,4}\b(\/[-a
 export function replaceUrls(string) {
   const target = string.replace(urlRegex, (match) => {
     if (match[0] !== 'h') {
-      return `http://${match}`;
+      match = `http://${match}`;
     }
-    return match;
+    return `<a href="#" onclick="window.open( '${match}', '_system');">${match}</a>`;
   });
-
-  return `<a href="#" onclick="window.open( '${target}', '_system');">${target}</a>`;
+  return target;
 }
