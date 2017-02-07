2a780ce2aa1f8332e6e7143d26051a5c0a0e9a4f
jacquesbonet <jacques.bonet@gmail.com>
2017-02-07 14:17:43 +0100

    - test server

--- scripts/reset.js
+++ scripts/reset.js
@@ -1,15 +1,11 @@
+import { MongoInternals } from 'meteor/mongo';
+
 if (process.env.NODE_ENV !== 'FUNC_TEST') {
   console.log( 'resetDatabase is not allowed outside of a development mode.');
 }
+else {
+   const db = MongoInternals.defaultRemoteCollectionDriver().mongo.db;
 
-let excludedCollections = ['system.indexes'];
-const db = MongoInternals.defaultRemoteCollectionDriver().mongo.db;
-const getCollections = Meteor.wrapAsync(db.collections, db);
-const collections = getCollections();
-const appCollections = _.reject(collections, col => col.collectionName.indexOf('velocity') === 0 ||
-      excludedCollections.indexOf(col.collectionName) !== -1);
-
-_.each(appCollections, (appCollection) => {
-  const remove = Meteor.wrapAsync(appCollection.remove, appCollection);
-  remove({});
-});
+   db.runCommand({dropDatabase: 1});
+   db.runCommand({dropAllUsersFromDatabase: 1});
+}
