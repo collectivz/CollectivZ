2cd87e1a4e482a52c79b970bfcd84eeb5cf88195
jacquesbonet <jacques.bonet@gmail.com>
2017-02-14 04:56:45 +0100

    Notification

--- imports/startup/client/notify.js
+++ imports/startup/client/notify.js
@@ -18,4 +18,15 @@ Meteor.startup(() => {
    });
 
    Push.enabled(true); // Will enable notifications (requires a token...)
+
+   Push.addListener('message', function(notification) {
+      // Called on every message
+      console.log(JSON.stringify(notification));
+   });
+
+   // Internal events
+   Push.addListener('token', function(token) {
+      // Token is { apn: 'xxxx' } or { gcm: 'xxxx' }
+      console.log(JSON.stringify(token));
+   });
 });
