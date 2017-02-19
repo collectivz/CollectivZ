ef0ee288f243e8eccec22021138a1ada941fce41
jacquesbonet <jacques.bonet@gmail.com>
2017-02-19 21:32:45 +0100

    apiKey

--- imports/startup/server/notify.js
+++ imports/startup/server/notify.js
@@ -1,65 +1,31 @@
 import { Meteor } from 'meteor/meteor';
-import { Push } from 'meteor/raix:push';
-
-Push.debug = true;
-
-Push.Configure({
-   apn: {
-//      certData: Assets.getText('apnDevCert.pem'),
-//      keyData: Assets.getText('apnDevKey.pem'),
-      passphrase: 'xxxxxxxxx',
-      production: true,
-      //gateway: 'gateway.push.apple.com',
-   },
-   gcm: {
-      apiKey: 'AIzaSyDf9leiVyhmfyqancbOGR0X7mno5zKWAnc',
-      projectNumber: 354968128746,
-   },
-   // production: true,
-   // 'sound' true,
-   // 'badge' true,
-   // 'alert' true,
-   // 'vibrate' true,
-   // 'sendInterval': 15000, Configurable interval between sending
-   // 'sendBatchSize': 1, Configurable number of notifications to send per batch
-   // 'keepNotifications': false,
-//
-});
-
-Push.allow({
-   send(userId, notification) {
-      return true; // Allow all users to send
-   },
-});
-
 
 Meteor.methods({
-   serverNotification(text, title) {
-      Push.send({
-         from: 'CollectivZ Global Notification',
-         title,
-         text,
-         badge: 12,
-         query: {},
-      });
-   },
-   userNotification(text, title, userId) {
-      Push.send({
-         from: 'CollectivZ User Notification',
-         title,
-         text,
-         badge: 12,
-         query: { userId },
-      });
-   },
-   tokenNotification(title, text, token) {
-      Push.send({
-         from: 'CollectivZ Token Notification',
-         title,
-         text,
-         badge: 12,
-         token,
-      });
-   },
+  serverNotification(channel, text) {
+    const notificationObj = { contents: { en: text }, included_segments: 'all' };
+
+    window.plugins.OneSignal.postNotification(notificationObj,
+         (successResponse) => {
+           console.log('Notification Post Success:', successResponse);
+         },
+         (failedResponse) => {
+           console.log('Notification Post Failed: ', failedResponse);
+           alert(`Notification Post Failed:\n${JSON.stringify(failedResponse)}`);
+         },
+      );
+  },
+  userNotification(text, userId) {
+    const notificationObj = { contents: { en: text }, include_player_ids: userId };
+
+    window.plugins.OneSignal.postNotification(notificationObj,
+         (successResponse) => {
+           console.log('Notification Post Success:', successResponse);
+         },
+         (failedResponse) => {
+           console.log('Notification Post Failed: ', failedResponse);
+           alert(`Notification Post Failed:\n${JSON.stringify(failedResponse)}`);
+         },
+      );
+  },
 });
 
