eb991ea505b9b033b139253aff68f0a244380db2
jacquesbonet <jacques.bonet@gmail.com>
2017-02-20 16:07:33 +0100

    oneSignalClient unresolved

--- imports/startup/server/notify.js
+++ imports/startup/server/notify.js
@@ -1,12 +1,7 @@
 import { Meteor } from 'meteor/meteor';
-
-var OneSignalClient = null;
+import OneSignalClient from 'node-onesignal';
 
 export function publish (data, options) {
-    // require the module
-    if (OneSignalClient === null)
-        OneSignalClient = require('node-onesignal');
-
     const client = new OneSignalClient('88cf61ed-a0b2-4303-98c6-114bb0991ddb', 'AIzaSyDf9leiVyhmfyqancbOGR0X7mno5zKWAnc');
 
     client.sendNotification(data, options);
