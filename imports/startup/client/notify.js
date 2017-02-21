f65913e0c0a70f1c1b918696e01d72bb8e6db2a0
jacquesbonet <jacques.bonet@gmail.com>
2017-02-21 22:19:23 +0100

    Notification
    Babel Stage 3

--- imports/startup/client/notify.js
+++ imports/startup/client/notify.js
@@ -1,1 +1,27 @@
 
+Meteor.startup(() => {
+   document.addEventListener('deviceready', function () {
+      // Enable to debug issues.
+      // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+
+      var notificationOpenedCallback = function(jsonData) {
+         console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+      };
+
+      var notificationIdsCallback = function(ids) {
+         console.log('getIds: ' + JSON.stringify(ids));
+      };
+
+      if (Meteor.isCordova) {
+         window.plugins.OneSignal
+            .startInit('354968128746')
+            .handleNotificationOpened(notificationOpenedCallback)
+            .getIds(notificationIdsCallback)
+            .endInit();
+      }
+
+      // Call syncHashedEmail anywhere in your app if you have the user's email.
+      // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
+      // window.plugins.OneSignal.syncHashedEmail(userEmail);
+   }, false);
+});
