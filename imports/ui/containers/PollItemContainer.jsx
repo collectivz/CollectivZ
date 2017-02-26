857d0525dea205d840a23113084978d7d490e6f5
jacquesbonet <jacques.bonet@gmail.com>
2017-02-27 00:04:04 +0100

    Suppression exception null _id lors d'envoi sondage

--- imports/ui/containers/PollItemContainer.jsx
+++ imports/ui/containers/PollItemContainer.jsx
@@ -5,7 +5,6 @@ import PollItem from "../components/chat/PollItem.jsx";
 
 export default createContainer(
   ({ poll }) => {
-    const propositionsSub = Meteor.subscribe("propositions", poll._id);
     const propositions = Propositions.find({ pollId: poll._id }).fetch();
 
     return {
