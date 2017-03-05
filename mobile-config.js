9cd0b1fd9d0924559fe8185a4540d51bdd694f48
jacquesbonet <jacques.bonet@gmail.com>
2017-03-05 18:13:53 +0100

    - correction flash ecran onboarding

--- mobile-config.js
+++ mobile-config.js
@@ -6,7 +6,7 @@ App.setPreference('android-targetSdkVersion', '21');
 App.info({
   id: 'com.union.collectivz',
   name: 'Digital Trait d\'Union',
-  version: '0.97',
+  version: '0.98',
 });
 
 App.icons({
